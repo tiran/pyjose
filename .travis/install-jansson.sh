bc75ed570e141d74ec3de1faaf1e186784f2d64b
Christian Heimes <cheimes@redhat.com>
2016-10-03 20:46:23 +0200

    jansson

--- .travis/install-jansson.sh
+++ .travis/install-jansson.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
-set -e
+set -ex
 VERSION=2.9
 
-if [ ! -d $PACKAGEHOME ]; then
-    exit 1
-fi
-
-TEMPDIR=${PACKAGEHOME}/tmp
-DEST=$TEMPDIR/jansson-${VERSION}.tar.gz
-
-trap "rm -rf $DEST $TEMPDIR/jansson-${VERSION}" ERR
-trap "rm -rf $TEMPDIR/jansson-${VERSION}" EXIT
-
-if [ -f $DEST ]; then
-    echo "jansson-${VERSION} is already installed"
-    exit 0
-fi
-
 echo "installing jansson-${VERSION}"
-
-mkdir -p $TEMPDIR
-cd $TEMPDIR
-
-wget http://www.digip.org/jansson/releases/jansson-${VERSION}.tar.gz
-
-tar xzf jansson-${VERSION}.tar.gz
-
+cd /tmp
+curl http://www.digip.org/jansson/releases/jansson-${VERSION}.tar.gz | tar -xz
 cd jansson-${VERSION}
-./configure --prefix=${PACKAGEHOME} --silent
+./configure --silent
 make
-make install
-
+sudo make install
 echo "installed jansson-${VERSION}"
